2351dce3cc10a4e556e72856b9d91f3993de0373
이유리 <leeri1211@naver.com>
2024-07-08 16:44:33 +0900

    FU-57 chore: initial setting
    개발 환경 세팅

---
+++ freebe/src/main/java/com/foru/freebe/FreebeApplication.java
@@ -0,0 +1,13 @@
+package com.foru.freebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreebeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreebeApplication.class, args);
+	}
+
+}
---
+++ freebe/src/test/java/com/foru/freebe/FreebeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foru.freebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreebeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

4fc78acda095ffc899fa49e43a37912c26172ddd
elwlahstmxjf@naver.com <jeong-yuseog@jeong-yuseog-ui-MacBookAir.local>
2024-07-17 19:11:53 +0900

    FU-77 feat: 상품관련 Entity 구현
    - 단방향 매핑으로 연관관계 설정

---
+++ src/main/java/com/foru/freebe/product/entity/Product.java
@@ -0,0 +1,24 @@
+package com.foru.freebe.product.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Product {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "product_id")
+	private Long id;
+
+	@NotNull
+	private Boolean activeStatus;
+}
---
+++ src/main/java/com/foru/freebe/product/entity/ProductImage.java
@@ -0,0 +1,32 @@
+package com.foru.freebe.product.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ProductImage {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotNull
+	private String thumbnailUrl;
+
+	@NotNull
+	private String originUrl;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "product_id")
+	private Product product;
+}

c8cc4ca81aae1ce09019a6c8b4fb9f32eca5e45e
elwlahstmxjf@naver.com <jeong-yuseog@jeong-yuseog-ui-MacBookAir.local>
2024-07-23 14:44:05 +0900

    FU-79 feat: add @Enumerated(EnumType.STRING) to enum fields

--- src/main/java/com/foru/freebe/product/entity/ProductDiscount.java
+++ src/main/java/com/foru/freebe/product/entity/ProductDiscount.java
@@ -2,6 +2,8 @@ package com.foru.freebe.product.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -26,6 +28,7 @@ public class ProductDiscount {
     @NotNull
     private String title;
 
+    @Enumerated(EnumType.STRING)
     @NotNull
     private DiscountType discountType;
 

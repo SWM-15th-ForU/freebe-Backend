7c99c8e9b0184b2b894f525e3c1c0c5b40a4fc29
이유리 <leeri1211@naver.com>
2024-07-24 18:27:30 +0900

    FU-54 feat: add SecurityConfig

---
+++ src/main/java/com/foru/freebe/config/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.foru.freebe.config;
+
+import static org.springframework.security.config.Customizer.*;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig {
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+			.csrf((csrf) -> csrf
+				.disable())
+			.cors(withDefaults())
+
+			.authorizeHttpRequests((request) -> request
+				.requestMatchers("/user/**").authenticated()
+				.requestMatchers("/user").hasAnyRole("USER")
+				.requestMatchers("/admin").hasAnyRole("ADMIN")
+				.anyRequest().permitAll())
+
+			.oauth2Login((oauth2) -> oauth2
+				.defaultSuccessUrl("/user")
+				.failureUrl("/"));
+		return http.build();
+	}
+}

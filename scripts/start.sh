3c04eb7cb57055bb2953f963e1a0d770bfff8573
정유석 <83908712+yuseok0215@users.noreply.github.com>
2024-07-24 20:33:18 +0900

    FU-63 feat: start.sh에서 jar 실행 명령어 변경

--- scripts/start.sh
+++ scripts/start.sh
@@ -13,7 +13,7 @@ echo "[$NOW] $JAR 복사" >> $START_LOG
 cp $ROOT_PATH/build/libs/freebe-0.0.1-SNAPSHOT.jar $JAR
 
 echo "[$NOW] > $JAR 실행" >> $START_LOG
-nohup java -jar $JAR > $APP_LOG 2> $ERROR_LOG &
+nohup java -jar $JAR &
 
 SERVICE_PID=$(pgrep -f $JAR)
 echo "[$NOW] > 서비스 PID: $SERVICE_PID" >> $START_LOG

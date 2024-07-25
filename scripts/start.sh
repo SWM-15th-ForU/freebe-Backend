785e10456648dee010d0359425941067f3c170d4
정유석 <83908712+yuseok0215@users.noreply.github.com>
2024-07-25 17:42:38 +0900

    FU-63 feat: start.sh에서 service_pid 찾는 명령어 변경

--- scripts/start.sh
+++ scripts/start.sh
@@ -15,5 +15,5 @@ cp $ROOT_PATH/build/libs/freebe-0.0.1-SNAPSHOT.jar $JAR
 echo "[$NOW] > $JAR 실행" >> $START_LOG
 nohup java -jar $JAR > $APP_LOG 2> $ERROR_LOG &
 
-SERVICE_PID=$(ps aux | grep "java -jar $JAR")
+SERVICE_PID=$(pgrep -f $JAR)
 echo "[$NOW] > 서비스 PID: $SERVICE_PID" >> $START_LOG

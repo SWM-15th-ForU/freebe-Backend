5c3dd1c3b2b981363ef77c8aa722b228c31ebb61
이유리 <leeri1211@naver.com>
2024-08-01 15:08:28 +0900

    FU-54 feat: jwt 관련 모델 구현

---
+++ src/main/java/com/foru/freebe/auth/model/JwtTokenEntity.java
@@ -0,0 +1,35 @@
+package com.foru.freebe.auth.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class JwtTokenEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "token_id")
+	private Long id;
+
+	@NotNull
+	private Long kakaoId;
+
+	@NotNull
+	private String refreshToken;
+
+	private JwtTokenEntity(Long kakaoId, String refreshToken) {
+		this.kakaoId = kakaoId;
+		this.refreshToken = refreshToken;
+	}
+
+	public static JwtTokenEntity createJwtToken(Long kakaoId, String refreshToken) {
+		return new JwtTokenEntity(kakaoId, refreshToken);
+	}
+}
---
+++ src/main/java/com/foru/freebe/auth/model/JwtTokenModel.java
@@ -0,0 +1,11 @@
+package com.foru.freebe.auth.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class JwtTokenModel {
+	private String accessToken;
+	private String refreshToken;
+}

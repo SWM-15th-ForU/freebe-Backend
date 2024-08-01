f84dd8c9dd5b5e3ad3f5e8ab263c40f7998adfe3
정유석 <elwlahstmxjf@naver.com>
2024-08-01 16:08:37 +0900

    FU-79 refactor: product 하위 서비스 클래스들을 삭제하고 ProductServiceImpl 내부에 private 메서드로 분리

--- src/main/java/com/foru/freebe/product/service/ProductImageService.java
+++
@@ -1,9 +0,0 @@
-package com.foru.freebe.product.service;
-
-import java.util.List;
-
-import com.foru.freebe.product.entity.Product;
-
-public interface ProductImageService {
-    void registerProductImage(List<String> productImageUrls, Product product);
-}
--- src/main/java/com/foru/freebe/product/service/ProductServiceImpl.java
+++ src/main/java/com/foru/freebe/product/service/ProductServiceImpl.java
@@ -1,11 +1,24 @@
 package com.foru.freebe.product.service;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import com.foru.freebe.common.dto.ApiResponseDto;
+import com.foru.freebe.product.dto.ProductComponentDto;
+import com.foru.freebe.product.dto.ProductDiscountDto;
+import com.foru.freebe.product.dto.ProductOptionDto;
 import com.foru.freebe.product.dto.ProductRegisterRequestDto;
 import com.foru.freebe.product.entity.Product;
+import com.foru.freebe.product.entity.ProductComponent;
+import com.foru.freebe.product.entity.ProductDiscount;
+import com.foru.freebe.product.entity.ProductImage;
+import com.foru.freebe.product.entity.ProductOption;
+import com.foru.freebe.product.respository.ProductComponentRepository;
+import com.foru.freebe.product.respository.ProductDiscountRepository;
+import com.foru.freebe.product.respository.ProductImageRepository;
+import com.foru.freebe.product.respository.ProductOptionRepository;
 import com.foru.freebe.product.respository.ProductRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -14,10 +27,10 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ProductServiceImpl implements ProductService {
     private final ProductRepository productRepository;
-    private final ProductImageService productImageService;
-    private final ProductComponentService productComponentService;
-    private final ProductOptionService productOptionService;
-    private final ProductDiscountService productDiscountService;
+    private final ProductImageRepository productImageRepository;
+    private final ProductComponentRepository productComponentRepository;
+    private final ProductOptionRepository productOptionRepository;
+    private final ProductDiscountRepository productDiscountRepository;
 
     @Override
     public ApiResponseDto<Void> registerProduct(ProductRegisterRequestDto productRegisterRequestDto) {
@@ -32,14 +45,14 @@ public class ProductServiceImpl implements ProductService {
         }
         productRepository.save(productAsActive);
 
-        productImageService.registerProductImage(productRegisterRequestDto.getProductImageUrls(), productAsActive);
-        productComponentService.registerProductComponent(productRegisterRequestDto.getProductComponents(),
+        registerProductImage(productRegisterRequestDto.getProductImageUrls(), productAsActive);
+        registerProductComponent(productRegisterRequestDto.getProductComponents(),
             productAsActive);
         if (productRegisterRequestDto.getProductOptions() != null) {
-            productOptionService.registerProductOption(productRegisterRequestDto.getProductOptions(), productAsActive);
+            registerProductOption(productRegisterRequestDto.getProductOptions(), productAsActive);
         }
         if (productRegisterRequestDto.getProductDiscounts() != null) {
-            productDiscountService.registerDiscount(productRegisterRequestDto.getProductDiscounts(), productAsActive);
+            registerDiscount(productRegisterRequestDto.getProductDiscounts(), productAsActive);
         }
 
         return ApiResponseDto.<Void>builder()
@@ -47,5 +60,51 @@ public class ProductServiceImpl implements ProductService {
             .message("Successfully added")
             .data(null)
             .build();
+    }
+
+    private void registerProductImage(List<String> productImageUrls, Product product) {
+        for (String productImageUrl : productImageUrls) {
+            ProductImage productImage = ProductImage.createProductImage(null, productImageUrl, product);
+            productImageRepository.save(productImage);
+        }
+    }
+
+    private void registerProductComponent(List<ProductComponentDto> productComponentDtoList, Product product) {
+        for (ProductComponentDto productComponentDto : productComponentDtoList) {
+            ProductComponent productComponent = ProductComponent.builder()
+                .title(productComponentDto.getTitle())
+                .content(productComponentDto.getContent())
+                .description(productComponentDto.getDescription())
+                .product(product)
+                .build();
+
+            productComponentRepository.save(productComponent);
+        }
+    }
+
+    private void registerProductOption(List<ProductOptionDto> productOptionDtoList, Product product) {
+        for (ProductOptionDto productOptionDto : productOptionDtoList) {
+            ProductOption productOption = ProductOption.builder()
+                .title(productOptionDto.getTitle())
+                .price(productOptionDto.getPrice())
+                .description(productOptionDto.getDescription())
+                .product(product)
+                .build();
+
+            productOptionRepository.save(productOption);
+        }
+    }
+
+    private void registerDiscount(List<ProductDiscountDto> productDiscountDtoList, Product product) {
+        for (ProductDiscountDto productDiscountDto : productDiscountDtoList) {
+            ProductDiscount productDiscount = ProductDiscount.builder()
+                .title(productDiscountDto.getTitle())
+                .discountType(productDiscountDto.getDiscountType())
+                .discountValue(productDiscountDto.getDiscountValue())
+                .description(productDiscountDto.getDescription())
+                .product(product)
+                .build();
+            productDiscountRepository.save(productDiscount);
+        }
     }
 }

ac8f6f80c50b8eba68d607a087ae470053630db5
이유리 <leeri1211@naver.com>
2024-08-01 14:36:54 +0900

    FU-54 feat: Cognito 의존성 및 코드 삭제
    Cognito에서 생성한 jwt 토큰을 validate하는 API가 별도로 존재하지 않고, payload를 커스텀 하기 어렵다는 이슈가 있음.

--- src/main/java/com/foru/freebe/auth/config/CustomAuthenticationSuccessHandler.java
+++ src/main/java/com/foru/freebe/auth/config/CustomAuthenticationSuccessHandler.java
@@ -3,7 +3,6 @@ package com.foru.freebe.auth.config;
 import java.io.IOException;
 import java.time.Duration;
 
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseCookie;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.core.user.OAuth2User;
@@ -11,7 +10,6 @@ import org.springframework.security.web.authentication.AuthenticationSuccessHand
 import org.springframework.stereotype.Component;
 
 import com.foru.freebe.auth.model.KakaoUser;
-import com.foru.freebe.auth.service.CognitoRegistrationService;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -19,30 +17,12 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @Component
 public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
-	private final CognitoRegistrationService cognitoRegistrationService;
-
-	public CustomAuthenticationSuccessHandler(CognitoRegistrationService cognitoRegistrationService) {
-		this.cognitoRegistrationService = cognitoRegistrationService;
-	}
-
 	@Override
 	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
 		Authentication authentication) throws IOException, ServletException {
 
 		OAuth2User oAuth2User = (OAuth2User)authentication.getPrincipal();
 		KakaoUser kakaoUser = new KakaoUser(oAuth2User);
-
-		cognitoRegistrationService.registerIfUserNotInCognito(kakaoUser);
-
-		String accessToken = cognitoRegistrationService.generateToken(kakaoUser).accessToken();
-		String refreshToken = cognitoRegistrationService.generateToken(kakaoUser).refreshToken();
-		//todo: refreshToken 저장 로직 추가
-
-		ResponseCookie accessTokenCookie = createCookie("accessToken", accessToken);
-		ResponseCookie responseTokenCookie = createCookie("refreshToken", refreshToken);
-
-		response.addHeader(HttpHeaders.SET_COOKIE, accessTokenCookie.toString());
-		response.addHeader(HttpHeaders.SET_COOKIE, responseTokenCookie.toString());
 
 		response.sendRedirect("/main");
 	}

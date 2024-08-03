ed296d02086add74102f9c187b5a32a109f11140
정유석 <elwlahstmxjf@naver.com>
2024-08-03 22:37:48 +0900

    FU-86 feat: member를 통해 product들을 조회하는 쿼리메서드 추가

--- src/main/java/com/foru/freebe/product/respository/ProductRepository.java
+++ src/main/java/com/foru/freebe/product/respository/ProductRepository.java
@@ -1,8 +1,12 @@
 package com.foru.freebe.product.respository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.foru.freebe.member.entity.Member;
 import com.foru.freebe.product.entity.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findByMember(Member member);
 }

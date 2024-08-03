e398d1b7b8011fd4e9c092d93f7c5ed4fb2a9899
정유석 <elwlahstmxjf@naver.com>
2024-08-03 22:34:26 +0900

    FU-86 feat: product와 member 단방향 연관관계 매핑

--- src/main/java/com/foru/freebe/product/entity/Product.java
+++ src/main/java/com/foru/freebe/product/entity/Product.java
@@ -1,12 +1,17 @@
 package com.foru.freebe.product.entity;
 
+import com.foru.freebe.member.entity.Member;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -29,6 +34,10 @@ public class Product {
     @Enumerated(EnumType.STRING)
     @NotNull
     private ActiveStatus activeStatus;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
 
     public Product(String title, String description, ActiveStatus activeStatus) {
         this.title = title;

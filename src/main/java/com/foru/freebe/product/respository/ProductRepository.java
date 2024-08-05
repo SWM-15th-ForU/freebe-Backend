e712fa17df491fb4a9c4f86e2de8adbfd6e1fc69
정유석 <elwlahstmxjf@naver.com>
2024-08-05 20:42:41 +0900

    FU-86 feat: 사진작가가 상품을 등록하지 않은 경우 널값이 반환되는 경우를 고려하여 Optional로 반환값을 포괄함

--- src/main/java/com/foru/freebe/product/respository/ProductRepository.java
+++ src/main/java/com/foru/freebe/product/respository/ProductRepository.java
@@ -1,6 +1,7 @@
 package com.foru.freebe.product.respository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,5 +9,5 @@ import com.foru.freebe.member.entity.Member;
 import com.foru.freebe.product.entity.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    List<Product> findByMember(Member member);
+    Optional<List<Product>> findByMember(Member member);
 }

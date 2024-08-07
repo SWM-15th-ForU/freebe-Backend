ac91e7f56edc293d9b0c4cd4c10ca527f5d638b7
이유리 <leeri1211@naver.com>
2024-08-07 15:53:38 +0900

    FU-54 feat: member <-> profile 일대일 연관관게 매핑

--- src/main/java/com/foru/freebe/member/entity/Member.java
+++ src/main/java/com/foru/freebe/member/entity/Member.java
@@ -4,13 +4,18 @@ import java.time.LocalDateTime;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import com.foru.freebe.profile.entity.Profile;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
@@ -25,8 +30,12 @@ import lombok.NoArgsConstructor;
 public class Member {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "user_id")
+    @Column(name = "member_id")
     private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "profile_id", referencedColumnName = "profile_id")
+    private Profile profile;
 
     @NotNull
     private Long kakaoId;
@@ -59,6 +68,10 @@ public class Member {
 
     public void updateRole(Role role) {
         this.role = role;
+    }
+
+    public void updateProfile(Profile profile) {
+        this.profile = profile;
     }
 
     @Builder
--- src/main/java/com/foru/freebe/profile/entity/Profile.java
+++ src/main/java/com/foru/freebe/profile/entity/Profile.java
@@ -1,9 +1,13 @@
 package com.foru.freebe.profile.entity;
 
+import com.foru.freebe.member.entity.Member;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -14,21 +18,29 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Profile {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "profile_id")
+    private Long id;
 
-	@NotNull
-	private String uniqueUrl;
+    @OneToOne(mappedBy = "profile")
+    private Member member;
 
-	private String introductionContent;
+    @NotNull
+    private String uniqueUrl;
 
-	private String bannerImageUrl;
+    private String introductionContent;
 
-	@Builder
-	public Profile(String uniqueUrl, String introductionContent, String bannerImageUrl) {
-		this.uniqueUrl = uniqueUrl;
-		this.introductionContent = introductionContent;
-		this.bannerImageUrl = bannerImageUrl;
-	}
+    private String bannerImageUrl;
+
+    public void updateUniqueUrl(String uniqueUrl) {
+        this.uniqueUrl = uniqueUrl;
+    }
+
+    @Builder
+    public Profile(String uniqueUrl, String introductionContent, String bannerImageUrl) {
+        this.uniqueUrl = uniqueUrl;
+        this.introductionContent = introductionContent;
+        this.bannerImageUrl = bannerImageUrl;
+    }
 }

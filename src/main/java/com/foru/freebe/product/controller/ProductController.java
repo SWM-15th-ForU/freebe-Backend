e0bc926041eded105c7d37867cb589281ef105b7
정유석 <elwlahstmxjf@naver.com>
2024-08-08 11:04:08 +0900

    FU-86 fix: 리소스를 대체하는 Put method로 수정

--- src/main/java/com/foru/freebe/product/controller/ProductController.java
+++ src/main/java/com/foru/freebe/product/controller/ProductController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,7 +35,7 @@ public class ProductController {
         return productService.getRegisteredProductList(memberId);
     }
 
-    @PostMapping("/update-status")
+    @PutMapping("/update-status")
     public ApiResponseDto<Void> updateProductActiveStatus(@RequestBody UpdateProductRequestDto requestDto) {
         return productService.updateProductActiveStatus(requestDto);
     }

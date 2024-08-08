9c161ba764247128e7726ff85e627a1d20c5f12e
정유석 <elwlahstmxjf@naver.com>
2024-08-08 14:22:50 +0900

    FU-104 chore: develop 브랜치에서 rebase 내용 추가 수정 #2

--- src/main/java/com/foru/freebe/errors/errorcode/CommonErrorCode.java
+++ src/main/java/com/foru/freebe/errors/errorcode/CommonErrorCode.java
@@ -1,7 +1,5 @@
 package com.foru.freebe.errors.errorcode;
 
-import org.springframework.http.HttpStatus;
-
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -9,11 +7,11 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum CommonErrorCode implements ErrorCode {
 
-    INVALID_PARAMETER(HttpStatus.BAD_REQUEST, "Invalid parameter included"),
-    RESOURCE_NOT_FOUND(HttpStatus.NOT_FOUND, "Resource not exists"),
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal server error"),
+    INVALID_PARAMETER(400, "Invalid parameter included"),
+    RESOURCE_NOT_FOUND(404, "Resource not exists"),
+    INTERNAL_SERVER_ERROR(500, "Internal server error"),
     ;
 
-    private final HttpStatus httpStatus;
+    private final int httpStatus;
     private final String message;
 }

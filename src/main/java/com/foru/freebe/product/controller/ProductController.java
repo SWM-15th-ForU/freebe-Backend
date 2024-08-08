5fa92ef5627ba43b38b9ec15f4ddb25173b69480
정유석 <elwlahstmxjf@naver.com>
2024-08-08 10:36:52 +0900

    FU-104 refactor: 패키지 변경으로 인한 import문 변경

--- src/main/java/com/foru/freebe/product/controller/ProductController.java
+++ src/main/java/com/foru/freebe/product/controller/ProductController.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.foru.freebe.common.dto.ApiResponseDto;
-import com.foru.freebe.product.dto.ProductRegisterRequestDto;
+import com.foru.freebe.common.dto.ApiResponse;
+import com.foru.freebe.product.dto.photographer.ProductRegisterRequestDto;
 import com.foru.freebe.product.service.ProductService;
 
 import lombok.RequiredArgsConstructor;
@@ -18,7 +18,7 @@ public class ProductController {
     private final ProductService productService;
 
     @PostMapping("/")
-    public ApiResponseDto<Void> registerProduct(@RequestBody ProductRegisterRequestDto productRegisterRequestDto) {
+    public ApiResponse<Void> registerProduct(@RequestBody ProductRegisterRequestDto productRegisterRequestDto) {
         return productService.registerProduct(productRegisterRequestDto);
     }
 }

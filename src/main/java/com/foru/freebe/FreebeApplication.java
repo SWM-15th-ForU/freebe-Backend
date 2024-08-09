accbf11b701653c9bb6d4b2e0d568bc1c7b646c9
이유리 <leeri1211@naver.com>
2024-08-09 22:22:38 +0900

    FU-134 fix: 컴포넌트 스캔 범위 지정

--- src/main/java/com/foru/freebe/FreebeApplication.java
+++ src/main/java/com/foru/freebe/FreebeApplication.java
@@ -3,11 +3,11 @@ package com.foru.freebe;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.foru.freebe")
 public class FreebeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FreebeApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FreebeApplication.class, args);
+    }
 
 }

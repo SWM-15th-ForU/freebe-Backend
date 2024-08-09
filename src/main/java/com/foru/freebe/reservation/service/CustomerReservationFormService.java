908845a5f3f36ff9be55abaff47ed7b4bdbfa46f
정유석 <elwlahstmxjf@naver.com>
2024-08-09 23:45:24 +0900

    FU-109 feat: 예약신청서 기본정보 조회 서비스 로직 추가

--- src/main/java/com/foru/freebe/reservation/service/CustomerReservationFormService.java
+++ src/main/java/com/foru/freebe/reservation/service/CustomerReservationFormService.java
@@ -1,5 +1,8 @@
 package com.foru.freebe.reservation.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.foru.freebe.common.dto.ApiResponse;
@@ -8,9 +11,13 @@ import com.foru.freebe.errors.errorcode.ProductErrorCode;
 import com.foru.freebe.errors.exception.RestApiException;
 import com.foru.freebe.member.entity.Member;
 import com.foru.freebe.member.repository.MemberRepository;
+import com.foru.freebe.product.dto.ProductComponentDto;
 import com.foru.freebe.product.entity.ActiveStatus;
 import com.foru.freebe.product.entity.Product;
+import com.foru.freebe.product.entity.ProductComponent;
+import com.foru.freebe.product.respository.ProductComponentRepository;
 import com.foru.freebe.product.respository.ProductRepository;
+import com.foru.freebe.reservation.dto.BasicReservationInfoResponse;
 import com.foru.freebe.reservation.dto.ReservationFormRequest;
 import com.foru.freebe.reservation.entity.ReservationForm;
 import com.foru.freebe.reservation.entity.ReservationStatus;
@@ -20,10 +27,11 @@ import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
-public class ReservationFormService {
+public class CustomerReservationFormService {
     private final ReservationFormRepository reservationFormRepository;
     private final MemberRepository memberRepository;
     private final ProductRepository productRepository;
+    private final ProductComponentRepository productComponentRepository;
 
     public ApiResponse<Void> registerReservationForm(ReservationFormRequest reservationFormRequest) {
         Member customer = memberRepository.findById(reservationFormRequest.getCustomerId())
@@ -56,6 +64,43 @@ public class ReservationFormService {
             .message("Good Request")
             .data(null)
             .build();
+    }
+
+    public ApiResponse<BasicReservationInfoResponse> getBasicReservationInfo(Long customerId, Long productId) {
+        Member customer = memberRepository.findById(customerId)
+            .orElseThrow(() -> new RestApiException(CommonErrorCode.RESOURCE_NOT_FOUND));
+
+        Product product = productRepository.findById(productId)
+            .orElseThrow(() -> new RestApiException(CommonErrorCode.RESOURCE_NOT_FOUND));
+
+        List<ProductComponent> productComponents = productComponentRepository.findByProduct(product);
+        List<ProductComponentDto> productComponentDtoList = convertProductComponentDtoList(
+            productComponents);
+
+        BasicReservationInfoResponse basicReservationInfoResponse = BasicReservationInfoResponse.builder()
+            .name(customer.getName())
+            .phoneNumber(customer.getPhoneNumber())
+            .productComponentDtoList(productComponentDtoList)
+            .build();
+
+        return ApiResponse.<BasicReservationInfoResponse>builder()
+            .status(200)
+            .message("Good Response")
+            .data(basicReservationInfoResponse)
+            .build();
+    }
+
+    private static List<ProductComponentDto> convertProductComponentDtoList(List<ProductComponent> productComponents) {
+        List<ProductComponentDto> productComponentDtoList = new ArrayList<>();
+        for (ProductComponent productComponent : productComponents) {
+            ProductComponentDto productComponentDto = ProductComponentDto.builder()
+                .title(productComponent.getTitle())
+                .content(productComponent.getContent())
+                .description(productComponent.getDescription())
+                .build();
+            productComponentDtoList.add(productComponentDto);
+        }
+        return productComponentDtoList;
     }
 
     private void validateActiveStatusOfProduct(ReservationFormRequest reservationFormRequest,

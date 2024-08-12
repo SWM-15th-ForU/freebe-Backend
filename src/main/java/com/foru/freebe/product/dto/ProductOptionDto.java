35bcc8e762f0ef1a829db981dc99506d638f2dbb
정유석 <elwlahstmxjf@naver.com>
2024-08-12 11:01:35 +0900

    FU-109 feat: ProductOptionDto 빌더패턴의 생성자 추가

--- src/main/java/com/foru/freebe/product/dto/ProductOptionDto.java
+++ src/main/java/com/foru/freebe/product/dto/ProductOptionDto.java
@@ -2,6 +2,7 @@ package com.foru.freebe.product.dto;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -14,4 +15,11 @@ public class ProductOptionDto {
     @Min(value = 1, message = "상품 옵션 가격은 0보다 큰 자연수여야 합니다.")
     private Integer price;
     private String description;
+
+    @Builder
+    public ProductOptionDto(String title, Integer price, String description) {
+        this.title = title;
+        this.price = price;
+        this.description = description;
+    }
 }

b90adf75c80fe97eaf80ded132e7dfcb6130081d
정유석 <elwlahstmxjf@naver.com>
2024-08-12 11:00:47 +0900

    FU-109 feat: BasicReservationInfoResponse에 상품 옵션 필드 추가

--- src/main/java/com/foru/freebe/reservation/dto/BasicReservationInfoResponse.java
+++ src/main/java/com/foru/freebe/reservation/dto/BasicReservationInfoResponse.java
@@ -3,6 +3,7 @@ package com.foru.freebe.reservation.dto;
 import java.util.List;
 
 import com.foru.freebe.product.dto.ProductComponentDto;
+import com.foru.freebe.product.dto.ProductOptionDto;
 
 import lombok.Builder;
 import lombok.Getter;
@@ -14,12 +15,15 @@ public class BasicReservationInfoResponse {
     private String name;
     private String phoneNumber;
     private List<ProductComponentDto> productComponentDtoList;
+    private List<ProductOptionDto> productOptionDtoList;
 
     @Builder
     public BasicReservationInfoResponse(String name, String phoneNumber,
-        List<ProductComponentDto> productComponentDtoList) {
+        List<ProductComponentDto> productComponentDtoList,
+        List<ProductOptionDto> productOptionDtoList) {
         this.name = name;
         this.phoneNumber = phoneNumber;
         this.productComponentDtoList = productComponentDtoList;
+        this.productOptionDtoList = productOptionDtoList;
     }
 }

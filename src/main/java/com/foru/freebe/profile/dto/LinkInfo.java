eff25ccabe22ab2cefed34f281914267db3368af
정유석 <elwlahstmxjf@naver.com>
2024-08-28 15:45:36 +0900

    FU-181 feat: ProfileResponse, LinkInfo 추가
    내부에 필드 추가를 위해 LinkInfo도 함께 추가했습니다.

---
+++ src/main/java/com/foru/freebe/profile/dto/LinkInfo.java
@@ -0,0 +1,11 @@
+package com.foru.freebe.profile.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class LinkInfo {
+    private String linkTitle;
+    private String linkUrl;
+}

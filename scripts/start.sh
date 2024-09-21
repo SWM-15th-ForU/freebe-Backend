87c70be150cdf78b82312604e392d03890c91b08
이유리 <leeri1211@naver.com>
2024-09-21 23:35:54 +0900

    FU-228 chore: CD 안정성을 위한 스크립트 개선
    - start.sh: 로그 메시지 구체화
    - stop.sh: 실행중인 프로세스 여러개를 모두 종료할 수 있도록 스크립트 수정 및 로그 메시지 구체화

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+# 환경 변수 로드
 set -o allexport
 source /home/ubuntu/freebe-backend/.env
 set +o allexport
 
+# 경로 및 파일 설정
 ROOT_PATH="/home/ubuntu/freebe-backend"
 JAR="$ROOT_PATH/application.jar"
 
@@ -13,11 +15,24 @@ START_LOG="$ROOT_PATH/start.log"
 
 NOW=$(date +%c)
 
-echo "[$NOW] $JAR 복사" >> $START_LOG
-cp $ROOT_PATH/build/libs/freebe-0.0.1-SNAPSHOT.jar $JAR
+# JAR 파일 복사
+echo "[$NOW] $JAR 복사 중..." >> $START_LOG
+if cp $ROOT_PATH/build/libs/freebe-0.0.1-SNAPSHOT.jar $JAR; then
+  echo "[$NOW] JAR 파일 복사 완료" >> $START_LOG
+else
+  echo "[$NOW] JAR 파일 복사 실패" >> $START_LOG
+  exit 1
+fi
 
+# 애플리케이션 실행
 echo "[$NOW] > $JAR 실행" >> $START_LOG
 nohup java -jar $JAR > $APP_LOG 2> $ERROR_LOG &
 
-SERVICE_PID=$(pgrep -f $JAR)
-echo "[$NOW] > 서비스 PID: $SERVICE_PID" >> $START_LOG
+# 실행된 프로세스의 PID 확인
+NEW_SERVICE_PID=$(pgrep -f $JAR)
+if [ ! -z "$NEW_SERVICE_PID" ]; then
+  echo "[$NOW] > 새로운 서비스 PID: $NEW_SERVICE_PID" >> $START_LOG
+else
+  echo "[$NOW] > 서비스 시작 실패" >> $START_LOG
+  exit 1
+fi

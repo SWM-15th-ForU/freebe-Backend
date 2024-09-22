aa0ca9b789ebcff59f69aaaf38e8abd3ea78341e
이유리 <124870949+rheeri@users.noreply.github.com>
2024-09-22 21:05:11 +0900

    [FU-228] CodeDeploy 배포 자동화 오류 분석 및 문제해결을 위한 스크립트 개선 (#36)
    * FU-228 chore: CD 안정성을 위한 스크립트 개선
    
    - start.sh: 로그 메시지 구체화
    - stop.sh: 실행중인 프로세스 여러개를 모두 종료할 수 있도록 스크립트 수정 및 로그 메시지 구체화
    
    * FU-228 chore: kill pid 명령어 로그파일에 기록
    
    kill 명령어가 실제로 어떻게 동작하는지 확인하기 위함
    
    * FU-228 chore: 로그 date 형식 수정
    
    * FU-228 chore: start.log, stop.log에 구분선 추가
    
    * FU-228 chore: stop.log 구분선 추가, ddl-auto 전략 임시 변경
    
    변경된 DB 구조 반영이 필요해 임시로 create로 설정
    
    * FU-228 chore: ddl-auto 전략 update로 원복
    
    * FU-228 feat: cd 이벤트 트리거에 pull_request 추가
    
    * FU-228 chore: CD 이벤트 트리거 원복
    
    이번 PR에서 수정한 스크립트로 배포 실행했을때 버그픽스가 되는지 검증하기 위해 일시적으로 pull_request를 CD 이벤트 트리거로 등록했었는데, 검증이 완료되어 다시 원복함
    
    ---------
    
    Co-authored-by: 정유석 <83908712+yuseok0215@users.noreply.github.com>

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,23 +1,39 @@
 #!/bin/bash
 
+# 환경 변수 로드
 set -o allexport
 source /home/ubuntu/freebe-backend/.env
 set +o allexport
 
+# 경로 및 파일 설정
 ROOT_PATH="/home/ubuntu/freebe-backend"
 JAR="$ROOT_PATH/application.jar"
 
 APP_LOG="$ROOT_PATH/application.log"
 ERROR_LOG="$ROOT_PATH/error.log"
 START_LOG="$ROOT_PATH/start.log"
+NOW=$(date "+%Y %b %d %a %H:%M:%S")
 
-NOW=$(date +%c)
+echo "--------------------------------------------------" >> $START_LOG
 
-echo "[$NOW] $JAR 복사" >> $START_LOG
-cp $ROOT_PATH/build/libs/freebe-0.0.1-SNAPSHOT.jar $JAR
+# JAR 파일 복사
+echo "[$NOW] $JAR 복사 중..." >> $START_LOG
+if cp $ROOT_PATH/build/libs/freebe-0.0.1-SNAPSHOT.jar $JAR; then
+  echo "[$NOW] JAR 파일 복사 완료" >> $START_LOG
+else
+  echo "[$NOW] JAR 파일 복사 실패" >> $START_LOG
+  exit 1
+fi
 
+# 애플리케이션 실행
 echo "[$NOW] > $JAR 실행" >> $START_LOG
 nohup java -jar $JAR > $APP_LOG 2> $ERROR_LOG &
 
-SERVICE_PID=$(pgrep -f $JAR)
-echo "[$NOW] > 서비스 PID: $SERVICE_PID" >> $START_LOG
+# 실행된 프로세스의 PID 확인
+NEW_SERVICE_PID=$(pgrep -f $JAR)
+if [ ! -z "$NEW_SERVICE_PID" ]; then
+  echo "[$NOW] > 새로운 서비스 PID: $NEW_SERVICE_PID" >> $START_LOG
+else
+  echo "[$NOW] > 서비스 시작 실패" >> $START_LOG
+  exit 1
+fi

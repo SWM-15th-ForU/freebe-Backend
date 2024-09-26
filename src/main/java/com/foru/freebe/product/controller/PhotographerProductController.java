baeee2ab39aca047c9f0f6006c2c4dbaf30f1885
정유석 <elwlahstmxjf@naver.com>
2024-09-26 20:29:59 +0900

    FU-217 fix: image 파일 최소 한 개이상 받도록 required=false 삭제

--- src/main/java/com/foru/freebe/product/controller/PhotographerProductController.java
+++ src/main/java/com/foru/freebe/product/controller/PhotographerProductController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -104,10 +105,10 @@ public class PhotographerProductController {
             .body(responseBody);
     }
 
-    @PutMapping("/product")
+    @PatchMapping("/product")
     public ResponseEntity<ResponseBody<Void>> updateProduct(
         @RequestPart(value = "request") UpdateProductDetailRequest request,
-        @RequestPart(value = "images", required = false) List<MultipartFile> images,
+        @RequestPart(value = "images") List<MultipartFile> images,
         @AuthenticationPrincipal MemberAdapter memberAdapter) throws IOException {
 
         Member photographer = memberAdapter.getMember();

e8c1205587b74d27e6ab5e1760048d0970e7e4c7
이유리 <leeri1211@naver.com>
2024-10-02 20:05:59 +0900

    FU-270 fix: 상품 상세 조회 시 썸네일 이미지 반환

--- src/main/java/com/foru/freebe/product/service/ProductDetailConvertor.java
+++ src/main/java/com/foru/freebe/product/service/ProductDetailConvertor.java
@@ -93,7 +93,7 @@ public class ProductDetailConvertor {
         List<ProductImage> productImages = productImageRepository.findByProduct(product);
         List<String> productImageUrls = new ArrayList<>();
         for (ProductImage productImage : productImages) {
-            productImageUrls.add(productImage.getOriginUrl());
+            productImageUrls.add(productImage.getThumbnailUrl());
         }
         return productImageUrls;
     }

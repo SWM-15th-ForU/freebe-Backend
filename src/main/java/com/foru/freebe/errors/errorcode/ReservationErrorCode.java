fb916d7d66d5de8583e6a18261bf51f0fe540062
정유석 <elwlahstmxjf@naver.com>
2024-10-08 16:34:29 +0900

    FU-268 refactor: ReservationErrorCode의 httpStatus 변경

--- src/main/java/com/foru/freebe/errors/errorcode/ReservationErrorCode.java
+++ src/main/java/com/foru/freebe/errors/errorcode/ReservationErrorCode.java
@@ -6,8 +6,8 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum ReservationErrorCode implements ErrorCode {
-    INVALID_STATUS_TRANSITION(400, "상태 전환이 불가능합니다"),
-    NO_RESERVATION_FORM(400, "해당하는 예약서가 존재하지 않습니다"),
+    INVALID_STATUS_TRANSITION(404, "상태 전환이 불가능합니다"),
+    NO_RESERVATION_FORM(404, "해당하는 예약서가 존재하지 않습니다"),
     INVALID_SHOOTING_DATE(400, "해당 날짜는 확정일자로 등록할 수 없습니다"),
     INVALID_SHOOTING_TIME(400, "해당 촬영시간은 확정일자로 등록할 수 없습니다"),
     INVALID_RESERVATION_STATUS(400, "현재 예약서 상태에서는 확정일자를 등록할 수 없습니다");

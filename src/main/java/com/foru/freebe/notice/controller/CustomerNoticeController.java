c5a1fbfdaeccd4a75cd3c15f26baf55ae28ecb35
정유석 <elwlahstmxjf@naver.com>
2024-10-13 21:19:36 +0900

    FU-278 refactor: @PathVariable로 변경

--- src/main/java/com/foru/freebe/notice/controller/CustomerNoticeController.java
+++ src/main/java/com/foru/freebe/notice/controller/CustomerNoticeController.java
@@ -5,8 +5,8 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.foru.freebe.common.dto.ResponseBody;
@@ -22,9 +22,9 @@ public class CustomerNoticeController {
 
     private final CustomerNoticeService customerNoticeService;
 
-    @GetMapping("/notice")
+    @GetMapping("/notice/{profileName}")
     public ResponseEntity<ResponseBody<List<NoticeDto>>> getNotices(
-        @RequestParam(value = "profileName") String profileName) {
+        @PathVariable("profileName") String profileName) {
 
         List<NoticeDto> responseData = customerNoticeService.getNotices(profileName);
 

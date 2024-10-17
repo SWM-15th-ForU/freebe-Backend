b96d81cc9f271e06bb8dd71bee7c69a9fe3fb2e8
이유리 <leeri1211@naver.com>
2024-10-17 12:04:41 +0900

    FU-244 fix: start.sh 에러출력 리렉션에서 공백 제거

--- scripts/start.sh
+++ scripts/start.sh
@@ -36,7 +36,7 @@ fi
 # 애플리케이션 실행
 echo "[$NOW] > $JAR 실행" >> $START_LOG
 if [ "$ENVIRONMENT" = "develop" ]; then
-  nohup java -jar $JAR > $APP_LOG 2 > $ERROR_LOG &
+  nohup java -jar $JAR > $APP_LOG 2> $ERROR_LOG &
 else
   nohup java -javaagent:$NEW_RELIC_JAR_FILE -Dnewrelic.config.license_key=$NEW_RELIC_LICENSE_KEY -Dnewrelic.config.app_name=$NEW_RELIC_APP_NAME -jar $JAR > $APP_LOG 2> $ERROR_LOG &
 fi

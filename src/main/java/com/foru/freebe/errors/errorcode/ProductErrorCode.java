2e52686fb7e76fbefe2daa2fa7a6b54032c95181
정유석 <elwlahstmxjf@naver.com>
2024-11-04 19:40:49 +0900

    FU-334 feat: 서버에서 리소스 접근 후 400번대 에러를 트래킹할 수 있도록 500 에러로 변경

--- src/main/java/com/foru/freebe/errors/errorcode/ProductErrorCode.java
+++ src/main/java/com/foru/freebe/errors/errorcode/ProductErrorCode.java
@@ -10,7 +10,7 @@ public enum ProductErrorCode implements ErrorCode {
     PRODUCT_INACTIVE_STATUS(400, "The product is currently inactive, so you can't register a booking form with it"),
     PRODUCT_ALREADY_EXISTS(400, "The product already exists, so it cannot be registered again"),
     COMPONENT_TITLE_ALREADY_EXISTS(400, "Component title already exists, so it cannot be registered again"),
-    PRODUCT_NOT_FOUND(404, "The product could not be found");
+    PRODUCT_NOT_FOUND(500, "The product could not be found");
 
     private final int httpStatus;
     private final String message;

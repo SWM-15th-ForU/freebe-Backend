e4b0e6cbd49d61bf5e1abfa2cec56e703fd08c33
이유리 <leeri1211@naver.com>
2024-12-20 15:31:38 +0900

    FU-346 feat: 날짜별 스케줄의 비즈니스 로직 검증 용 테스트코드 작성

--- src/main/java/com/foru/freebe/schedule/dto/DailyScheduleRequest.java
+++ src/main/java/com/foru/freebe/schedule/dto/DailyScheduleRequest.java
@@ -6,6 +6,7 @@ import java.time.LocalTime;
 import com.foru.freebe.schedule.entity.ScheduleStatus;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -23,4 +24,12 @@ public class DailyScheduleRequest {
 
     @NotNull(message = "End time must not be null")
     private LocalTime endTime;
+
+    @Builder
+    public DailyScheduleRequest(ScheduleStatus scheduleStatus, LocalDate date, LocalTime startTime, LocalTime endTime) {
+        this.scheduleStatus = scheduleStatus;
+        this.date = date;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
 }
